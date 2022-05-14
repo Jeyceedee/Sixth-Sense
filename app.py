c98dbfd90bbc8990548ec99036fd44c19ed6cc97
RamilSC <73583424+RamilSC@users.noreply.github.com>
2022-05-15 00:42:45 +0800

    feat: Added Embed Frames

--- app.py
+++ app.py
@@ -19,5 +19,21 @@ def sidebar():
     return render_template('sidebar.html')
 
 
+# Content Pages
+
+@app.route("/admindashboard")
+def adminDashboard():
+    return render_template('adminDashboard.html')
+
+
+@app.route("/patientRegistration")
+def patientRegistration():
+    return render_template('patientRegistration.html')
+
+@app.route("/patientList")
+def patientList():
+    return render_template('patientList.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
